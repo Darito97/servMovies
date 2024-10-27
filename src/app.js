275f408171616d940116d3139a75410590c0679c
David Alonso <davidalonso991@gmail.com>
2024-10-27 12:44:05 -0600

    Agregado de morgan para logs de peticiones

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 /* Routers */
 import RouterFavs from "./routes/favs.routes.js";
@@ -9,6 +10,8 @@ dotenv.config();
 const App = express();
 
 App.use(express.json());
+App.use(morgan("dev"));
+
 App.use("/favs", RouterFavs);
 App.use("/movies", RouterMovies);
 
